e42d683379a2bdfd4e43b3983e5f4442761601d4
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-16 19:00:44 -0300

    feat: add user input and calculate max income
    The program will now ask the user for the cinema dimensions and calculate the maximum income possible

--- cinemaroom/main.go
+++ cinemaroom/main.go
@@ -6,12 +6,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Cinema:")
-	room := generateCinema(8, 9)
-	printCinemaLayout(room)
+	var rows, seats int
+	fmt.Println("Enter the number of rows:")
+	_, _ = fmt.Scan(&rows)
+	fmt.Println("Enter the number of seats in each row:")
+	_, _ = fmt.Scan(&seats)
+
+	income := calculateMaximumIncome(rows, seats)
+	fmt.Println("Total income:")
+	fmt.Printf("$%d\n", income)
 }
 
-func generateCinema(rows, columns int) [][]string {
+func GenerateCinema(rows, columns int) [][]string {
 
 	cinema := make([][]string, rows)
 	for i := range cinema {
@@ -30,11 +36,23 @@ func generateCinema(rows, columns int) [][]string {
 	return cinema
 }
 
-func printCinemaLayout(cinema [][]string) {
+func PrintCinemaLayout(cinema [][]string) {
 	for i := 0; i < len(cinema); i++ {
 		for j := 0; j < len(cinema[i]); j++ {
 			fmt.Printf("%s ", cinema[i][j])
 		}
 		fmt.Println()
+	}
+}
+
+func calculateMaximumIncome(rows, seats int) int {
+	if rows*seats < 60 {
+		return rows * seats * 10
+	} else {
+		if rows%2 == 0 {
+			return 8*(rows/2)*seats + 10*(rows/2)*seats
+		} else {
+			return 8*(rows/2+1)*seats + 10*(rows/2)*seats
+		}
 	}
 }
